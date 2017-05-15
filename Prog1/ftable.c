2bfcdbb35615a01f7057b1b8258681439b33be2d
agrazela14 <agrazela@gmail.com>
2017-05-15 12:22:29 -0700

    Putting the actual ftable on here, cuz it was only on the server previously

--- Prog1/ftable.c
+++ Prog1/ftable.c
@@ -15,8 +15,8 @@ int main(int argc, char **argv) {
     args.verbose = 0;
     args.skip = 0;
     args.period = 1;
-    args.inFile = stdin;//STDIN_FILENO;
-    args.outFile = stdout;STDOUT_FILENO;
+    args.inFile = stdin;
+    args.outFile = stdout;
     checkArgs(argc, argv, &args);
 
     table(&args);
@@ -25,11 +25,14 @@ int main(int argc, char **argv) {
 }
 
 void table(arguments *args) {
-    char curChar = 0;//[args->period]; 
-    int alphabetCount[ALPHABETLEN] = {};
-    int skipNdx = 0, periodNdx = args->period;
-    //int readBytes = 0;// = fread(plainText, 1, BUFFSIZE, args->inFile); 
+    char curChar = 0; 
+    int alphabetCount[ALPHABETLEN];
+    int skipNdx = 0, periodNdx = args->period, ndx = 0;
     int totalRead = 0;
+
+    for (ndx = 0; ndx < ALPHABETLEN; ndx++) {
+        alphabetCount[ndx] = 0;
+    }
     
     while (curChar != EOF && skipNdx < args->skip) { 
         curChar = fgetc(args->inFile);
@@ -52,43 +55,13 @@ void table(arguments *args) {
         if (periodNdx < args->period - 1) {
             periodNdx++;
         }
-        else {
+        else if (isalpha(curChar)) {
+            curChar = toupper(curChar);
             alphabetCount[curChar - 'A']++;
             totalRead += 1;
             periodNdx = 0;
         }
     }
-
-
-    /*
-    if (args->skip > 0) {
-        for(ndx = 0; ndx > args->skip; ndx++) {
-            fgetc(args->inFile);
-        }
-    }
-    
-    while ((curChar = fgetc(args->inFile)) != EOF) {
-        curChar = toupper(curChar);
-        if (curChar <= 'Z' && curChar >= 'A') {
-            alphabetCount[curChar - 'A']++;
-            totalRead += 1;
-        }
-        for (ndx = 0; ndx < args->period - 1; ndx++) {
-            fgetc(args->inFile);
-        }
-    }
-    */
-    
-    /*     
-    do {
-        readBytes = read(args->inFile, curChar, args->period);
-        curChar[0] = toupper(curChar[0]);
-        if (curChar[0] <= 'Z' && curChar[0] >= 'A') {
-            alphabetCount[curChar[0] - 'A']++;
-            totalRead += 1;
-        }
-    } while (readBytes == args->period);
-    */
     printout(alphabetCount, totalRead, args);
 }
 
@@ -104,19 +77,14 @@ void printout(int *alphabetTable, int totalRead, arguments *args) {
 
     sprintf(buf, "Total chars: %d\n", totalRead);
     fputs(buf, args->outFile);
-    //write(args->outFile, buf, strlen(buf));
+    if (totalRead == 0) {
+        totalRead = 1;
+    }
     for (i = 0; i < ALPHABETLEN; i++) {
         sprintf(buf, "%C:% 9d (%5.2lf%%) %.*s\n", 'A' + i, alphabetTable[i], 
          100 * ((double)alphabetTable[i] / totalRead), 
           (int)ceil((double)alphabetTable[i] / totalRead * 100), asterisk);
-        /*
-        for (int j = 0; j < alphabetTable[i]; j++) {
-            sprintf(buf + strlen(buf) - 1, "*");
-        }
-        sprintf(buf + strlen(buf), "\n");
-        */
         fputs(buf, args->outFile);
-        //write(args->outFile, buf, strlen(buf));
     }
 }
 
